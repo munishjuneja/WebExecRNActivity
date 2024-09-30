0c9404d972779c5464e10efe34e3ea63fdae864e
Munish juneja <mjuneja29@gmail.com>
2024-09-30 10:38:06 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import LandingPage from './src/LandingPage';
+import store from './src/store';
+import {Provider} from 'react-redux';
+import AppNavigation from './src/MyAppNavigation';
+
+function App() {
+  return <AppNavigation />;
+}
+
+export default App;
